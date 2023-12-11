b63d5e1f9a88236cf43f13048c5afc4bb5a0d36c
tomohiro ueno <hagurono9@gmail.com>
2023-12-11 14:02:18 +0900

    add

---
+++ lib/view_models/main_view_model.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meditaion/generated/models/managers/ad_manager.dart';
+import 'package:flutter_meditaion/generated/models/managers/in_app_purchase_manager.dart';
+import 'package:flutter_meditaion/generated/models/managers/sound_manager.dart';
+import 'package:flutter_meditaion/generated/models/repositories/shared_prefs_repository.dart';
+
+class MainViewModel extends ChangeNotifier {
+  MainViewModel({
+    required this.sharedPrefsRepository,
+    required this.soundManager,
+    required this.adManager,
+    required this.inAppPurchaseManager,
+  });
+
+  final SharedPrefsRepository sharedPrefsRepository;
+  final SoundManager soundManager;
+  final AdManager adManager;
+  final InAppPurchaseManager inAppPurchaseManager;
+}
